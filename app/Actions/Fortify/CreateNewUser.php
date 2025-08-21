f379ecd76463d60ff4b27c04619d211c6526ca8e
rizalaji1st <rizalaji561@gmail.com>
2025-08-21 09:25:15 +0700

    Add-fitur : RBAC Spatie Jetstream

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,10 +26,12 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        return tap(User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-        ]);
+        ]), function (User $user) {
+            $user->assignRole('manajer');
+        });
     }
 }
