85ad7fce1c5f78ae8449ebcb0aa718efb0372daf
rizalaji1st <rizalaji561@gmail.com>
2025-09-05 14:27:45 +0700

    Fix : Handle login dulu

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,14 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -19,4 +21,17 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            // Kalau user belum login
+            if (!auth()->check()) {
+                // simpan intended url untuk redirect setelah login
+                session(['url.intended' => $request->fullUrl()]);
+
+                // arahkan ke login
+                return redirect()->route('login');
+            }
+
+            // Kalau user sudah login, tetap tampilkan 404 normal
+            return response()->view('errors.404', [], 404);
+        });
     })->create();
