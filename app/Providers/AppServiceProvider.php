1421e1651cc21a2e2ff5c204d1ea4512ac873040
rizalaji1st <rizalaji561@gmail.com>
2025-09-05 21:08:42 +0700

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+
+        // Pastikan default collation sesuai server
+        Schema::defaultStringLength(191);
+        Schema::defaultMorphKeyType('string');
     }
 }
